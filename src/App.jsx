75c6e8dc3965e95b00dc1d59a873ae6d0d9e2ba7
Olga <minaeva9@gmail.com>
2024-11-16 15:30:09 -0500

    Card component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
+import Card from "./components/shared/Card";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
