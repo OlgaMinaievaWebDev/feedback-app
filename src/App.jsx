2e914183afcff1f597af08d1d6e1297d5b18599d
Olga <minaeva9@gmail.com>
2024-11-17 15:33:01 -0500

    add event to delete feedback

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import Card from "./components/shared/Card";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <div>
         <Header />
         <div className="container">
-          <FeedbackList feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
       </div>
     </>
