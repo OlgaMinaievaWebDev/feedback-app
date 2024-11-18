2ca0fd663ad4ccfd14640c7d2ea3d83dc6577ea6
Olga <minaeva9@gmail.com>
2024-11-18 13:39:26 -0500

    handleTextChange validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,25 +4,38 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState(" ");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
     <Card>
       <form>
-        <h2>
-          How would you rate your service with us?
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="Write your review"
-              onChange={handleTextChange}
-              value={text}
-            />
-            <Button type="submit">Send</Button>
-          </div>
-        </h2>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Write your review"
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
