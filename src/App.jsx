98395bd9e56877dad5eaabbe1481a85c9d31ae28
Olga <minaeva9@gmail.com>
2024-11-18 13:14:32 -0500

    Form input and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+
+import FeedbackData from "./data/FeedbackData";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -18,6 +20,7 @@ function App() {
       <div>
         <Header />
         <div className="container">
+          <FeedbackForm />
           <FeedbackStats feedback={feedback} />
           <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form>
+        <h2>
+          How would you rate your service with us?
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Write your review"
+              onChange={handleTextChange}
+              value={text}
+            />
+            <button type="submit">Send</button>
+          </div>
+        </h2>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
