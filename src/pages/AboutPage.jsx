641e2ac54c56edf712a14b61e0c44dde9e5a56ba
Olga <minaeva9@gmail.com>
2024-11-20 13:15:18 -0500

    created Link

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import Card from "../components/shared/Card";
+
 function AboutPage() {
-  return <div>About</div>;
+  return (
+    <Card>
+      <div>
+        <h1>About this Project</h1>
+        <p>This is React App to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <Link to="/">Back to Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
 }
 
 export default AboutPage;
