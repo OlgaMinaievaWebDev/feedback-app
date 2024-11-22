5edcc15acbf298203a4345559683d6e5acd902b7
Olga <minaeva9@gmail.com>
2024-11-22 16:13:37 -0500

    add feedback

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -17,16 +17,25 @@ export const FeedbackProvider = ({ children }) => {
 
   //fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch("http://localhost:3000/feedback");
+    const response = await fetch("/feedback?_sort=id&_order=desc");
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
   };
 
   //add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   //delete feedback
